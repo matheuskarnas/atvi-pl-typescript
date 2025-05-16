78d726d5930258291952351276009b870fc169cc
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-05-16 12:17:08 -0300

    feat: adicionar suporte para múltiplos RGs no cadastro de clientes

--- src/negocio/cadastroCliente.ts
+++ src/negocio/cadastroCliente.ts
@@ -2,6 +2,7 @@ import Entrada from "../io/entrada";
 import Cliente from "../modelo/cliente";
 import CPF from "../modelo/cpf";
 import Cadastro from "./cadastro";
+import RG from "../modelo/rg";
 
 export default class CadastroCliente extends Cadastro {
   private clientes: Array<Cliente>;
@@ -31,7 +32,26 @@ export default class CadastroCliente extends Cadastro {
     let dia = new Number(partesData[0].valueOf()).valueOf();
     let dataEmissao = new Date(ano, mes, dia);
     let cpf = new CPF(valor, dataEmissao);
-    let cliente = new Cliente(nome, nomeSocial, cpf);
+
+    let newRG = true
+        let rgs: Array<RG> = []
+        while (newRG) {
+            let rgValor = this.entrada.receberTexto(`Por favor informe o número do rg: `);
+            let rgData = this.entrada.receberTexto(`Por favor informe a data de emissão do rg, no padrão dd/mm/yyyy: `);
+            let partesRGData = rgData.split('/')
+            let anoRG = new Number(partesRGData[2].valueOf()).valueOf()
+            let mesRG = new Number(partesRGData[1].valueOf()).valueOf()
+            let diaRG = new Number(partesRGData[0].valueOf()).valueOf()
+            let dataEmissaoRG = new Date(anoRG, mesRG, diaRG)
+            let rg = new RG(rgValor, dataEmissaoRG);
+            rgs.push(rg)
+            let continuar = this.entrada.receberTexto(`Deseja cadastrar mais um rg? (s/n) `)
+            if (continuar.toLowerCase() == 'n') {
+                newRG = false
+            }
+        }
+        
+    let cliente = new Cliente(nome, nomeSocial, cpf, rgs);
     this.clientes.push(cliente);
     console.log(`\nCadastro concluído :)\n`);
   }
