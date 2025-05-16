33f11df671ef31ffa99be32cb4da04ac7bea7970
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-05-16 12:18:40 -0300

    feat: adicionar exibição da data de cadastro na listagem de clientes

--- src/negocio/listagemClientes.ts
+++ src/negocio/listagemClientes.ts
@@ -12,6 +12,9 @@ export default class ListagemClientes extends Listagem {
     this.clientes.forEach((cliente) => {
       console.log(`Nome: ` + cliente.nome);
       console.log(`Nome social: ` + cliente.nomeSocial);
+      console.log(
+        `Data de cadastro: ` + cliente.getDataCadastro.toLocaleDateString()
+      );
       console.log(`CPF: ` + cliente.getCpf.getValor);
       console.log(
         `RGs: \n${cliente.getRgs
